2f62d8e741bf06f12674e8d674d6ebc5d8330f65
Mark O Regan <R00157116@citonline.ie>
2018-02-21 10:12:44 +0000

    first journey test

---
+++ tests/journeytest.py
@@ -0,0 +1,10 @@
+import unittest
+
+from src import journey as jny
+
+class JourneyTest(unittest.TestCase):
+
+    def test_distance(self):
+        journey = jny.Journey(starting_point=[0, 0], end_point=[17, 0])
+        distance = journey.journey_distance(20)
+        self.assertEquals(340, distance)
