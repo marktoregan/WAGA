98160f6b73d1c41923241ae369b4ea0efd5e69fc
mark <mark.t.oregan@mycit.ie>
2018-03-21 13:28:02 +0000

    is there local problems again

--- src/samples2.py
+++ src/samples2.py
@@ -52,16 +52,30 @@ journeys.append(stop5)
 sorted_by_arrival_time = sorted(journeys, key=lambda k: k['arrival_time'])
 
 #print(f'{sorted_by_arrival_time}')
-#print(f'{groups}')
 
 
-def calcualte_stops_waits(wait_time, charge_point):
+def print_totals(arrival_time_array):
+    total_time = 0
+    print( f'ar {arrival_time_array}' )
+
+    for x in arrival_time_array:
+        print(x)
+        arrive = x["arrival_time"]
+        depart = x["departure_time"]
+        diff = depart - arrive
+        total_min = diff.total_seconds() / 60
+        total_time += total_min
+        print(f'total of one group {total_time}')
+
+
+
+def calculate_stops_waits(wait_time, charge_point):
     for idx, val in enumerate(charge_point):
         if idx == 0:
             arrival_time = val['arrival_time']
             departure_time_wait = arrival_time + timedelta(minutes=wait_time)
             val['departure_time'] = departure_time_wait
-            #print(f"wait {idx} or {wait_time}")
+            print(f"wait {idx} or {wait_time}")
         else:
             arrival_time = val['arrival_time']
             previous_depart_time = charge_point[idx-1]['departure_time']
@@ -77,30 +91,14 @@ def calcualte_stops_waits(wait_time, charge_point):
             #print(f'wait {idx} and {departure_time_wait}')
 
 
+
 for group, items in groupby(sorted_by_arrival_time, key=lambda x: x['ev_point_id']):
-    #print(f'Group {group} and items {list(items)}')
     c_point = list(items)
-    #print(f'{c_point}')
-    calcualte_stops_waits(25, list(items))
-    #for thing in items:
-     #   print (f' list(thing) ')
-
-
+    calculate_stops_waits(25, list(items))
+    print_totals(list(items))
 
 print("#################")
 print("calculate total time")
-
-
-def print_totals():
-    total_time = 0
-    for x in sorted_by_arrival_time:
-        print(x)
-        arrive = x["arrival_time"]
-        depart = x["departure_time"]
-        diff = depart - arrive
-        total_min = diff.total_seconds() / 60
-        total_time += total_min
-        print(total_time)
 
 
 #print_totals()
