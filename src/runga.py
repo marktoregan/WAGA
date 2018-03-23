32c9717e194eebd8b3151bf50eb9527be757e94d
Mark O Regan <R00157116@citonline.ie>
2018-03-23 16:13:55 +0000

    population printing

--- src/runga.py
+++ src/runga.py
@@ -5,32 +5,32 @@ from datetime import datetime
 
 
 if __name__ == '__main__':
-    journeymanager = jm.JourneyManager()
+    journey_manager = jm.JourneyManager()
     start_time = datetime.now()
     journey1 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey1)
+    journey_manager.add_journey(journey1)
     journey2 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey2)
+    journey_manager.add_journey(journey2)
     journey3 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey3)
+    journey_manager.add_journey(journey3)
     journey4 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey4)
+    journey_manager.add_journey(journey4)
     journey5 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey5)
+    journey_manager.add_journey(journey5)
     journey6 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey6)
+    journey_manager.add_journey(journey6)
     journey7 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey7)
+    journey_manager.add_journey(journey7)
     journey8 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey8)
+    journey_manager.add_journey(journey8)
     journey9 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey9)
+    journey_manager.add_journey(journey9)
     journey10 = jy.Journey(start_time=start_time)
-    journeymanager.add_journey(journey10)
+    journey_manager.add_journey(journey10)
 
     available_stops = ['a','b','c','d','e']
 
-    population = pop.Population(journey_manager=journeymanager, available_stops=available_stops, population_size=20, initialise=True)
+    population = pop.Population(journey_manager=journey_manager, available_stops=available_stops, population_size=20, initialise=True)
 
     for p in population.journey_allocations:
         print(f'{p.journey_allocation}')
