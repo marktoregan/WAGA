352b381fd75835ed800592029da853db1f12b425
Mark O Regan <R00157116@citonline.ie>
2018-04-09 10:36:47 +0100

    removed calculateminutes and tests

--- tests/wagatests.py
+++ tests/wagatests.py
@@ -12,12 +12,4 @@ class WagaTests(unittest.TestCase):
     gene_set = ['a', 'b', 'c', 'd', 'e']
 
     def test_10_journeys_5_points(self):
-        x = ga.Waga()
-
-        optimal_fitness, best = x.solve(['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e', 'a', 'b', 'c','d','e'], 10)
-        self.assertTrue(not optimal_fitness > best.Fitness)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+        pass
