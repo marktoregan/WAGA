de73d6491e12137202bd8306d033ab20f1aa20db
mark <mark.t.oregan@mycit.ie>
2018-04-16 19:50:41 +0100

    some printing

--- src/runga.py
+++ src/runga.py
@@ -2,39 +2,41 @@ from src import population as p, geneticalgorithm as gen, benchmark as bk, popul
 
 
 
-if __name__ == '__main__':
+class RunGA():
 
-    jm = pjm.PopulateJourneyManager()
-    journey_manager = jm.get_journey_manager(20)
-    available_stops = ['a', 'b', 'c', 'd', 'e']
+    def  process(self):
 
-    pop = p.Population(journey_manager=journey_manager,
-                                available_stops=available_stops,
-                                population_size=20,
-                                initialise=True)
+        jm = pjm.PopulateJourneyManager()
+        journey_manager = jm.get_journey_manager(20)
+        available_stops = ['a', 'b', 'c', 'd', 'e']
 
-    print(f'fittest')
-    fittest_allocation = pop.get_fittest()
-    print(f"Initial fitness: {fittest_allocation.get_fitness()} stops {fittest_allocation.journey_allocation}")
+        pop = p.Population(journey_manager=journey_manager,
+                                    available_stops=available_stops,
+                                    population_size=20,
+                                    initialise=True)
 
-    ga = gen.GeneticAlgorithm(journey_manager)
-    pop = ga.evolvePopulation(pop)
-    for i in range(0, 100):
+        print(f'fittest')
+        fittest_allocation = pop.get_fittest()
+        print(f"Initial fitness: {fittest_allocation.get_fitness()} stops {fittest_allocation.journey_allocation}")
+
+        ga = gen.GeneticAlgorithm(journey_manager)
         pop = ga.evolvePopulation(pop)
-        print(f'generation: {i} and best score: {pop.get_fittest().get_fitness()}')
+        for i in range(0, 100):
+            pop = ga.evolvePopulation(pop)
+            print(f'generation: {i} and best score: {pop.get_fittest().get_fitness()}')
 
-    # Print final results
-    print("Finished")
-    print(f"Final distance {pop.get_fittest().get_fitness()} ")
-    print(f"Solution: {pop.get_fittest().get_fitness()} and the EVP allocated {pop.get_fittest().journey_allocation}")
-    
-    my_dict = {i:pop.get_fittest().journey_allocation.count(i) for i in pop.get_fittest().journey_allocation}
-    print(f'count {my_dict}')
-    bench = bk.Benchmark(journey_manager=journey_manager.stops, stops=available_stops)
+        # Print final results
+        print("Finished")
+        print(f"Final distance {pop.get_fittest().get_fitness()} ")
+        print(f"Solution: {pop.get_fittest().get_fitness()} and the EVP allocated {pop.get_fittest().journey_allocation}")
 
-    print(f'Random soultion: {bench.get_random_fitness()} and {bench.random_allocation}')
+        my_dict = {i:pop.get_fittest().journey_allocation.count(i) for i in pop.get_fittest().journey_allocation}
+        print(f'count {my_dict}')
+        bench = bk.Benchmark(journey_manager=journey_manager.stops, stops=available_stops)
+
+        print(f'Random soultion: {bench.get_random_fitness()} and {bench.random_allocation}')
 
 
-    my_dict_random = {i: bench.random_allocation.count(i) for i in bench.random_allocation}
+        my_dict_random = {i: bench.random_allocation.count(i) for i in bench.random_allocation}
 
-    print(f'count {my_dict_random}')
+        print(f'count {my_dict_random}')
