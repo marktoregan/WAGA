341461b94535d423b29aa668392e206c37af41f6
mark <mark.t.oregan@mycit.ie>
2018-04-23 10:52:09 +0100

    pseudo code generation

---
+++ src/psudocode/journey-Psudocode.py
@@ -0,0 +1,72 @@
+import math
+import random
+from datetime import datetime, timedelta
+from src.config import mapconfig as md
+from src import journeystop as js, ev_charge_point as evp
+
+
+CLASS Journey(object):
+    FUNCTION __init__(self, **kwargs):
+        """
+        Constructor
+        :param kwargs: start_time, starting_point, end_point, stops
+        """
+         start_time <- kwargs.get("start_time", datetime.now())
+         starting_point <- kwargs.get("starting_point")
+         end_point <- kwargs.get("end_point")
+         current_location <- kwargs.get("current_point")
+         total_journey_time <- kwargs.get("total_journey_time", 0)
+         stop <- kwargs.get("stop")
+    ENDFUNCTION
+
+ENDCLASS
+
+
+    FUNCTION _euclidean_distance(self, point1, point2):
+        """
+        :param point1:
+        :param point2:
+        :return:
+        """
+        euclidean <- math.sqrt(((point1[0]-point2[0])**2)+((point1[1]-point2[1])**2))
+        RETURN euclidean
+    ENDFUNCTION
+
+
+    FUNCTION distance_in_minutes(self, speed_limit):
+        """
+        :param ev1:
+        :return: time <- distance / speed
+        """
+        distance_km <-  distance()
+        time <- distance_km / speed_limit
+        time *= 60
+        RETURN time
+    ENDFUNCTION
+
+
+    FUNCTION distance(self):
+        """
+        :return:
+        """
+        ev_stop <- evp.EvChargePoint()
+        ev_stop <- ev_stop.get_ev_charge_point( stop[0])
+        distance_to_evp_km <-  _distance_in_km( starting_point, ev_stop.location)
+        distance_from_evp_km <-  _distance_in_km(ev_stop.location,  end_point)
+        total_distance_km <- distance_to_evp_km + distance_from_evp_km
+        RETURN total_distance_km
+    ENDFUNCTION
+
+
+    FUNCTION _distance_in_km(self, p1, p2):
+        mpd <- md.MapConfig()
+        dist <-  _euclidean_distance(p1,p2)
+        distance_in_km <- dist * mpd.legend_distance
+        RETURN distance_in_km
+    ENDFUNCTION
+
+
+    FUNCTION set_journey_stop(self, stops):
+        s <- random.choice(stops)
+        a_stop <- js.JourneyStop(s)
+         stop.append(a_stop)
---
+++ src/psudocode/population-Psudocode.py
@@ -0,0 +1,54 @@
+from src import journeyallocation as ja
+import random
+
+
+CLASS Population(object):
+ENDCLASS
+
+
+    FUNCTION __init__(self, **kwargs):
+         journey_allocations <- []
+         population_size <- kwargs.get("population_size", 0)
+         available_stops <- kwargs.get("available_stops", [])
+         journey_manager <- kwargs.get("journey_manager", [])
+    ENDFUNCTION
+
+
+        for i in range(0,  population_size):
+             journey_allocations.append(None)
+        ENDFOR
+
+         initialise <- kwargs.get("initialise", False)
+        IF  initialise:
+            for i in range(0,  population_size):
+                new_journey_all <- ja.JourneyAllocation(journey_manager= journey_manager,
+                                                       available_stops= available_stops)
+                new_journey_all.generate_individual()
+                 save_journey_allocation(i, new_journey_all)
+        ENDIF
+            ENDFOR
+
+    FUNCTION size_of_population(self):
+        RETURN len( journey_allocations)
+    ENDFUNCTION
+
+
+    FUNCTION save_journey_allocation(self, index, journey):
+         journey_allocations[index] <- journey
+    ENDFUNCTION
+
+
+    FUNCTION get_journey_allocation(self, index):
+        RETURN  journey_allocations[index]
+    ENDFUNCTION
+
+
+    FUNCTION get_fittest(self):
+        fittest <-  journey_allocations[0]
+        for i in range(0,  population_size):
+            journ <-  get_journey_allocation(i)
+            IF fittest.get_fitness() >= journ.get_fitness():
+                fittest <-  get_journey_allocation(i)
+            ENDIF
+        ENDFOR
+        RETURN fittes
