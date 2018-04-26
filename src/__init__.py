3d10abb85986e174a4e5fa33a838ffb2db90ec10
mark <mark.t.oregan@mycit.ie>
2018-04-26 14:04:49 +0100

    remove pickle

--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     print(datetime.now())
     #stop_ids = evcps.EvChargePoints.get_by_type("Fast AC Type-2 44kW")
     #run = rga.RunGA(generations=10, population_size=1000)
-    run = rga.RunGA(generations=1, population_size=10)
+    run = rga.RunGA(generations=100, population_size=1000)
     run.process()
     print(datetime.now())
--- src/calc/loadpickle.py
+++
@@ -1,4 +0,0 @@
-import pickle
-
-favorite_color = pickle.load( open( "save.p", "rb" ) )
-print(favorite_color)
