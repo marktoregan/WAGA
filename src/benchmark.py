8bdba8d0673189ed4b60f76dd275e028cbbc813b
mark <mark.t.oregan@mycit.ie>
2018-04-27 07:59:19 +0100

    removed files and refernce to pyproj in convertdata

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,30 +1,9 @@
 from scipy.spatial import distance
 from src import evchargepoint as evp
-
 from src.calc import distance as dis
-
-#from mpl_toolkits.basemap import Basemap
-from numpy import random
-
-
-#h = dis.Distance([-6.270447, 53.339791], [-2.991028, 53.402061])
-#
-
-#h2 = dis.Distance([53.339479, -6.241077],[53.016218, -7.300986])
-
-#h2 = dis.Distance([-6.241077, 53.339479],[-7.300986, 53.016218])
-
-#Curriersbog
-#Co. Laois
-
-#53.016218, -7.300986
-
-#print(h2.km)
-
 
 p1 = [0, 0]
 p2 = [10, 10]
-
 
 
 def midpoints(p1, p2):
@@ -44,8 +23,6 @@ def closest_node(node, nodes):
     closest_index = distance.cdist([node], nodes).argmin()
     return nodes[closest_index]
 
-#a = random.randint(1000, size=(50000, 2))
-
 cpoints = evp.EvChargePoint()
 
 x = cpoints.all_ev_charge_points()
@@ -64,22 +41,3 @@ p = closest_node(some_pt, x)
 print(p)
 
 
-
-#print(x2, y2)
-
-
-#all_ev_charge_points
-
-
-#Treasury Building
-#Grand Canal Street Lower, Grand Canal Dock, Dublin 2
-
-#53.339479, -6.241077
-
-#Curriersbog
-#Co. Laois
-
-#53.016218, -7.300986
-
-
-#{"_default": {"1": {"evp": "mnwmp00vzsyr-0", "charge_type": "Type-2 AC Socket 3.7kW", "location": [0.0004859709617290923, -6.789089625130105e-05], "charge_time_required": 480, "longitude": 54.09804, "latitude": -7.55758, "name": "Slieve Russell Hotel, Belturbet-Ballyconnell Road (N87), Ballyconnell, County Cavan"},
--- src/evaluation/evaluations.py
+++ src/evaluation/evaluations.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from src.calc import distance as dis, midpoint as mp
+from src.calc import distance as dis
 # eval 1
 #ga v neariest
 
@@ -19,22 +19,11 @@ from src.calc import distance as dis, midpoint as mp
 
 Coordinate = namedtuple('Coordinate', ['longitute', 'latitude'])
 
-h = dis.Distance(Coordinate(longitute=-6.270447, latitude=53.339791),
-             Coordinate(longitute=-2.991028, latitude=53.402061))
-print(h.km)
 
 DublinCity = Coordinate(latitude=53.338313, longitute=-6.238713)
 Portlaoise = Coordinate(latitude=53.032791, longitute=-7.298212)
 
-n = dis.Distance(DublinCity,Portlaoise)
-print(n.km)
 
 mid = mp.MidPoint(DublinCity,Portlaoise)
 midway = mid.calc_midpoint()
 
-n1 = dis.Distance(DublinCity, midway)
-print(n1.km)
-
-
-n2 = dis.Distance(Portlaoise, midway)
-print(n2.km)
