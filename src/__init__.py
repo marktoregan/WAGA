08a1cd09aa124dcf6b06eeaa0513617b65af6e5c
mark <mark.t.oregan@mycit.ie>
2018-05-14 13:15:40 +0100

    added return

--- src/__init__.py
+++ src/__init__.py
@@ -13,29 +13,31 @@ if __name__ == '__main__':
 
     fitness_mean =[]
     bench_mean = []
-    num_journeys =5
+    num_journeys =10
     totals=[]
     result_lst = []
     Results = namedtuple('Results', 'journney_num fit_mean bench_mean')
     #for j in range(0,10):
     fitness = []
     bench = []
-    for i in range(0,10):
-        print(f'on {i} of 10')
-        #run = rga.RunGA(generations=100, population_size=50, num_of_journeys=100, initialise=True)
-        run = rga.RunGA(generations=200, population_size=100, num_of_journeys=num_journeys, initialise=True)
-        fit, ben = run.process(stopIDs)
-        fitness.append(fit)
-        bench.append(ben)
-        res = RunResult(id=num_journeys, fitness=fit, bench=ben)
-        result_lst.append(res)
-        num_journeys += 10
-
-    print(f'whats {result_lst}')
-    fit_mean = reduce(lambda x, y: x + y, fitness) / len(fitness)
-    bench_mean =reduce(lambda x, y: x + y, bench) / len(bench)
-    res = [num_journeys, fit_mean, bench_mean]
-    totals.append(res)
+    runs = 7
+    number_of_times = 10
+    for j in range(0,runs):
+        for i in range(0,number_of_times):
+            print(f'on {i} of {number_of_times} and {j} of {runs}')
+            #run = rga.RunGA(generations=100, population_size=50, num_of_journeys=100, initialise=True)
+            run = rga.RunGA(generations=200, population_size=30, num_of_journeys=num_journeys, initialise=True)
+            fit, ben = run.process(stopIDs)
+            fitness.append(fit)
+            bench.append(ben)
+            res = RunResult(id=num_journeys, fitness=fit, bench=ben)
+            result_lst.append(res)
+        print(f'whats {result_lst}')
+        fit_mean = reduce(lambda x, y: x + y, fitness) / len(fitness)
+        bench_mean =reduce(lambda x, y: x + y, bench) / len(bench)
+        res = [num_journeys, fit_mean, bench_mean]
+        totals.append(res)
+        num_journeys += 30
     print(f'jour: {i} fit: {fit_mean} bench: {bench_mean}')
     print(totals)
 
--- src/runga.py
+++ src/runga.py
@@ -17,37 +17,39 @@ class RunGA(object):
         self.generations = kwargs.get("generations", 10)
 
     def process(self, charge_types):
-
         self.available_stops, preloaded_point_details = evcps.EvChargePoints.get_ev_charge_point_by_ids(charge_types)
-        #print(self.available_stops, preloaded_point_details)
         distances_loaded = self.method_name(preloaded_point_details)
         pre_loaded = {'evp_details':preloaded_point_details, 'distances':distances_loaded}
 
-        ##print(f'Getting Initial Population.')
-        #print(f'just before {len(self.journey_manager.stops)}')
         pop = p.Population(journey_manager=self.journey_manager,
                                     available_stops=self.available_stops,
                                     population_size=self.population_size,
                                     initialise=self.initialise,
                                     preloaded_stops=pre_loaded)
-        #print(f'Getting Journey manager.')
         ga = gen.GeneticAlgorithm(self.journey_manager)
-        #print(f'Starting evolve.')
-        #print('any before here??')
         pop = ga.evolve_population(pop)
         generation_results = []
 
-        current_res = 2
+        current = None
+        prev = None
+        converge_count = 0
+        convered_at = 0
+
         for i in range(0, self.generations):
+            if current == prev:
+                converge_count += 1
+            else:
+                converge_count = 1
+            if converge_count == 20:
+                convered_at = i
+                break
+
             pop = ga.evolve_population(pop)
             res = pop.get_fittest().get_fitness(pop.preloaded_stops)
             generation_results.append(res)
-            print(f"gen {i} {generation_results[i]} ")
-        #print(generation_results)
-        #print(pop)
-        #print(f'stops {len(pop.get_fittest().journey_allocation)}')
-        #print(f'stops {pop.get_fittest().journey_allocation}')
-        #print(f'fitness: {pop.get_fittest().get_fitness(pop.preloaded_stops)}')
+            current = res
+            prev = generation_results[i - 1]
+            #print(f"gen {i} {generation_results[i]} ")
 
         bench_charge_types = charge_types[:]
         ben = bk.Benchmark(journey_manager=self.journey_manager, charge_types=bench_charge_types)
